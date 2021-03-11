c23c1d0d9cde7c8a0fb4c4f3fdc0a120cd3e70fc
Julian Ravn Thrap-Meyer <Julian.Ravn.Thrap-Meyer@nav.no>
2021-03-11 15:36:03 +0100

    Legger til tester på degradering av kvalitet

--- Kotlin/src/test/kotlin/com/gildedrose/GoodsTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GoodsTest.kt
@@ -4,13 +4,40 @@ import com.gildedrose.Goods.Companion.asGoods
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
+/*
+	- All items have a SellIn value which denotes the number of days we have to sell the item
+	- All items have a Quality value which denotes how valuable the item is
+	- At the end of each day our system lowers both values for every item
+
+Pretty simple, right? Well this is where it gets interesting:
+
+	- Once the sell by date has passed, Quality degrades twice as fast
+	- The Quality of an item is never negative
+	- "Aged Brie" actually increases in Quality the older it gets
+	- The Quality of an item is never more than 50
+	- "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+	- "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
+	Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
+	Quality drops to 0 after the concert
+
+ */
+
 internal class GoodsTest {
 
     @Test
-    fun `All items have a SellIn value which denotes the number of days we have to sell the item`() {
+    fun `Each day lowers SellIn and Quality`() {
         val item = Item("Sword", sellIn = 6, quality = 3)
         item.asGoods().age()
         assertEquals(5, item.sellIn)
+        assertEquals(2, item.quality)
     }
+
+    @Test
+    fun `Once the sell by date has passed, Quality degrades twice as fast`() {
+        val item = Item("Sword", sellIn = 0, quality = 3)
+        item.asGoods().age()
+        assertEquals(1, item.quality)
+    }
+
 
 }

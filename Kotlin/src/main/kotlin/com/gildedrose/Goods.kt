80ecadb1c6bc78b25143cd2b4a941ab8904cbaa9
Julian Ravn Thrap-Meyer <Julian.Ravn.Thrap-Meyer@nav.no>
2021-03-11 15:33:42 +0100

    Flytter oppdatering av SellIn øverst

--- Kotlin/src/main/kotlin/com/gildedrose/Goods.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/Goods.kt
@@ -2,6 +2,10 @@ package com.gildedrose
 
 class Goods private constructor(private val item: Item) {
     fun age() {
+        if (item.name != "Sulfuras, Hand of Ragnaros") {
+            item.sellIn = item.sellIn - 1
+        }
+
         if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
             if (item.quality > 0) {
                 if (item.name != "Sulfuras, Hand of Ragnaros") {
@@ -13,23 +17,19 @@ class Goods private constructor(private val item: Item) {
                 item.quality = item.quality + 1
 
                 if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-                    if (item.sellIn < 11) {
+                    if (item.sellIn < 10) {
                         if (item.quality < 50) {
                             item.quality = item.quality + 1
                         }
                     }
 
-                    if (item.sellIn < 6) {
+                    if (item.sellIn < 5) {
                         if (item.quality < 50) {
                             item.quality = item.quality + 1
                         }
                     }
                 }
             }
-        }
-
-        if (item.name != "Sulfuras, Hand of Ragnaros") {
-            item.sellIn = item.sellIn - 1
         }
 
         if (item.sellIn < 0) {

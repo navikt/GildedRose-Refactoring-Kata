af71ab6b9ba1d8ac5b198798f94be2e09bb7586b
Julian Ravn Thrap-Meyer <Julian.Ravn.Thrap-Meyer@nav.no>
2021-03-11 15:25:32 +0100

    Flipper if-logikk på backstage-pass-sjekk

--- Kotlin/src/main/kotlin/com/gildedrose/Goods.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/Goods.kt
@@ -38,14 +38,14 @@ class Goods private constructor(private val item: Item) {
                     item.quality = item.quality + 1
                 }
             } else {
-                if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
+                if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+                    item.quality = item.quality - item.quality
+                } else {
                     if (item.quality > 0) {
                         if (item.name != "Sulfuras, Hand of Ragnaros") {
                             item.quality = item.quality - 1
                         }
                     }
-                } else {
-                    item.quality = item.quality - item.quality
                 }
             }
         }

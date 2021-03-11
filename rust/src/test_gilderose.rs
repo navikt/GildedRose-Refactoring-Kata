5025835fdcf7fec2389177bfccb6811e7ea86471
Kent Daleng <kent.daleng@nav.no>
2021-03-11 14:48:28 +0100

    add backstage passes test

--- rust/src/test_gilderose.rs
+++ rust/src/test_gilderose.rs
@@ -56,6 +56,29 @@ pub fn sulfuras() {
 }
 
 #[test]
+pub fn backstage_passes() {
+    let items = vec![Item::new("Backstage passes to a TAFKAL80ETC concert", 15, 20)];
+    let mut rose = GildedRose::new(items);
+
+    rose.update_quality();
+    assert_eq!(14, rose.items[0].sell_in);
+    assert_eq!(21, rose.items[0].quality);
+
+    (0..5).for_each(|_| rose.update_quality());
+    assert_eq!(9, rose.items[0].sell_in);
+    assert_eq!(27, rose.items[0].quality);
+
+    (0..5).for_each(|_| rose.update_quality());
+    assert_eq!(4, rose.items[0].sell_in);
+    assert_eq!(38, rose.items[0].quality);
+
+    (0..5).for_each(|_| rose.update_quality());
+    assert_eq!(-1, rose.items[0].sell_in);
+    assert_eq!(0, rose.items[0].quality);
+}
+
+
+#[test]
 pub fn common_items() {
     assert_common_item("Elixir of the Mongoose");
     assert_common_item("+5 Dexterity Vest");

af30a8e347389c2d13a0a945a9c414f68088e468
Malaz Alkoj <malaz.alkoj@nav.no>
2021-03-11 16:23:53 +0100

    coding dojo , kata

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -3,28 +3,26 @@ package com.gildedrose
 class GildedRose(var items: Array<Item>) {
 
     fun behandleBackstage(item: Item) {
-        when (item.sellIn ) {
-         in 10 .. Int.MAX_VALUE -> incQuality(item.quality)
+        when (item.sellIn) {
+            in 10..Int.MAX_VALUE -> item.quality += 1
+            in 5..9 -> item.quality += 2
+            in 1..4 -> item.quality += 3
+            else -> item.quality = 0
+        }
     }
 
     fun behandleBrie(item: Item) {
-        if (item.quality < 50 ) {
-            item.quality += 1
-        }
+        if (item.sellIn > 0) item.quality += 1
+        else item.quality -= 2
+    }
 
     fun behandleDefault(item: Item) {
-        if (item.quality > 50 ) {
-            item.quality += 1
+        if (item.quality in 1..49) {
+            if (item.sellIn > 0) item.quality -= 1
+            else item.quality -= 2
         }
     }
 
-    fun incQuality(value: Int, change: Int = 1) = if (value < 50 ) {
-            value + change
-        } else value
-
-    fun decQuality(value: Int, change: Int = 1) = if (value > 0 ) {
-        value - change
-    } else value
 
     fun updateQuality() {
         val sulfuras = "Sulfuras, Hand of Ragnaros"
@@ -33,57 +31,15 @@ class GildedRose(var items: Array<Item>) {
 
         for (item in items) {
 
+            if (item.name == sulfuras) continue
             item.sellIn = item.sellIn - 1
-
             when (item.name) {
                 backstage -> behandleBackstage(item)
                 brie -> behandleBrie(item)
-                sulfuras -> continue
                 else -> behandleDefault(item)
             }
-
-
-
-
-            if (item.name != brie && item.name != backstage) {
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1
-
-                    if (item.name == backstage) {
-                        if (item.sellIn < 10) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1
-                            }
-                        }
-
-                        if (item.sellIn < 5) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (item.sellIn < 0) {
-                if (item.name != brie) {
-                    if (item.name != backstage) {
-                        if (item.quality > 0) {
-                            item.quality = item.quality - 1
-                        }
-                    } else {
-                        item.quality = item.quality - item.quality
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1
-                    }
-                }
-            }
+            if (item.quality > 50) item.quality = 50
+            if (item.quality < 0) item.quality = 0
         }
     }
 

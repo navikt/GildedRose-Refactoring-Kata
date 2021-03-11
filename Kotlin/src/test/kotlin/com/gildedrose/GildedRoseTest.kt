5b0d0eaf51f959ee340abc747256b1f0cea8890a
Per Jørgen Walstrøm <per.jorgen.walstrom@nav.no>
2021-03-11 14:22:22 +0100

    failing test on brie

--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -3,18 +3,50 @@ package com.gildedrose
 import com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemOut
 import org.approvaltests.Approvals
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 internal class GildedRoseTest {
 
-    @Test @Disabled
+    @Test
     fun foo() {
         val items = arrayOf<Item>(Item("foo", 0, 0))
         val app = GildedRose(items)
         app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
+        assertEquals("foo", app.items[0].name)
+    }
 
+    @Test
+    fun `quality can not be negative`() {
+        val items = arrayOf(Item("foo", 0, 0))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertTrue(app.items[0].quality >= 0)
+    }
+
+    @Test
+    fun `quality degrades by one when sell by date is reduced by one`() {
+        val items = arrayOf(Item("foo", 1, 4))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(3, app.items[0].quality)
+    }
+
+    @Test
+    fun `quality degrades twice as fast when sell by date is passed`() {
+        val items = arrayOf(Item("foo", 0, 4))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(2, app.items[0].quality)
+    }
+
+    @Test
+    fun `brie increases in quality over time`() {
+        val items = arrayOf(Item("Aged Brie", 0, 4))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(2, app.items[0].quality)
     }
 
     @Test

7acfde06d819242a92946b59a4b94cf4b0df9608
Kent Daleng <kent.daleng@nav.no>
2021-03-11 14:37:28 +0100

    add common items

--- rust/src/test_gilderose.rs
+++ rust/src/test_gilderose.rs
@@ -1,5 +1,26 @@
 use crate::{GildedRose, Item};
 
+pub fn assert_common_item(name: &str) {
+    let items = vec![Item::new(name, 5, 20)];
+    let mut rose = GildedRose::new(items);
+
+    rose.update_quality();
+    assert_eq!(4, rose.items[0].sell_in);
+    assert_eq!(19, rose.items[0].quality);
+
+    (0..4).for_each(|_| rose.update_quality());
+    assert_eq!(0, rose.items[0].sell_in);
+    assert_eq!(15, rose.items[0].quality);
+
+    rose.update_quality();
+    assert_eq!(-1, rose.items[0].sell_in);
+    assert_eq!(13, rose.items[0].quality);
+
+    (0..99).for_each(|_| rose.update_quality());
+    assert_eq!(-100, rose.items[0].sell_in);
+    assert_eq!(0, rose.items[0].quality);
+}
+
 //- "Aged Brie" actually increases in Quality the older it gets
 //- The Quality of an item is never more than 50
 #[test]
@@ -22,4 +43,10 @@ pub fn aged_brie() {
     (0..5).for_each(|_| rose.update_quality());
     assert_eq!(-6, rose.items[0].sell_in);
     assert_eq!(14, rose.items[0].quality);
+}
+
+#[test]
+pub fn common_items() {
+    assert_common_item("Elixir of the Mongoose");
+    assert_common_item("+5 Dexterity Vest");
 }

6a02d195e07423bf4314f93b7c274c2b2359db67
Kent Daleng <kent.daleng@nav.no>
2021-03-11 15:45:59 +0100

    refactor: split out brie

--- rust/src/gildedrose.rs
+++ rust/src/gildedrose.rs
@@ -32,50 +32,48 @@ impl GildedRose {
 
     pub fn update_quality(&mut self) {
         for item in &mut self.items {
-	    match item.name.as_str() {
-		"Sulfuras, Hand of Ragnaros" => (),
-		_ => update_common(item)
-	    }
-	}
+            match item.name.as_str() {
+                "Sulfuras, Hand of Ragnaros" => (),
+                "Aged Brie" => update_brie(item),
+                _ => update_common(item),
+            }
+        }
+    }
+}
+
+fn update_brie(item: &mut Item) {
+    item.sell_in -= 1;
+
+    if item.quality < 50 {
+        item.quality += 1;
+        if item.sell_in < 0 {
+            item.quality += 1;
+        }
     }
 }
 
 fn update_common(item: &mut Item) {
-    if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" {
-	if item.quality > 0 {
-	    item.quality -= 1;
-	}
-    } else {
-	if item.quality < 50 {
-	    item.quality += 1;
-
-	    if item.name == "Backstage passes to a TAFKAL80ETC concert" {
-		if item.sell_in < 11 && item.quality < 50 {
-		    item.quality += 1;
-		}
-
-		if item.sell_in < 6 && item.quality < 50 {
-		    item.quality += 1;
-		}
-	    }
-	}
+    if item.name != "Backstage passes to a TAFKAL80ETC concert" && item.quality > 0 {
+        item.quality -= 1;
+    } else if item.quality < 50 {
+        item.quality += 1;
+        if item.name == "Backstage passes to a TAFKAL80ETC concert" {
+            if item.sell_in < 11 && item.quality < 50 {
+                item.quality += 1;
+            }
+            if item.sell_in < 6 && item.quality < 50 {
+                item.quality += 1;
+            }
+        }
     }
 
     item.sell_in -= 1;
 
     if item.sell_in < 0 {
-	if item.name != "Aged Brie" {
-	    if item.name != "Backstage passes to a TAFKAL80ETC concert" {
-		if item.quality > 0 {
-		    item.quality -= 1;
-		}
-	    } else {
-		item.quality = 0;
-	    }
-	} else {
-	    if item.quality < 50 {
-		item.quality += 1;
-	    }
-	}
+        if item.name != "Backstage passes to a TAFKAL80ETC concert" && item.quality > 0 {
+            item.quality -= 1;
+        } else {
+            item.quality = 0;
+        }
     }
 }

5c347b2a62ed6b9396bfd602f6c27b1ce32d77c4
erlendaxelsson <erlend.axelsson@gmail.com>
2021-03-11 14:06:07 +0100

    Lagt til golden master test

--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -1,17 +1,28 @@
 package com.gildedrose
 
+import com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemOut
+import org.approvaltests.Approvals
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 internal class GildedRoseTest {
 
-    @Test
+    @Test @Disabled
     fun foo() {
         val items = arrayOf<Item>(Item("foo", 0, 0))
         val app = GildedRose(items)
         app.updateQuality()
         assertEquals("fixme", app.items[0].name)
 
+    }
+
+    @Test
+    fun `golden master`(){
+        val actual = tapSystemOut {
+            main(arrayOf())
+        }
+        Approvals.verify(actual)
     }
 
 }
